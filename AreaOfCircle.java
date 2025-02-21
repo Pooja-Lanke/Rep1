587e37ae96403332c6200c1033d93ee63153062e
pooja Nichit <poojaknichit@gmail.com>
2025-02-22 01:03:33 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,10 @@
+class AreaOfCircle{
+public static void main(String[] args){
+int radius = 7;
+
+double result=3.14*radius*radius;
+System.out.println("Area of circle is: "+result);
+
+}
+
+}
---
+++ SumOfNumber.java
@@ -0,0 +1,12 @@
+Q. 2 Assignment 1
+class SumOfNumber
+{
+  public static void main(String[] args)
+  {
+   int a=50;
+     int b=60;
+       System.out.println("Addition is " +(a+b));
+    }
+}
+
+Output:110
