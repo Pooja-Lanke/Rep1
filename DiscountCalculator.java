8da32a3213feda3c24a7cc81298c0e6b3890f68f
pooja Nichit <poojaknichit@gmail.com>
2025-02-22 00:17:49 +0530

    Add files via upload

---
+++ DiscountCalculator.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class DiscountCalculator{ 
+    public static void main(String[] args) { 
+    
+Scanner sc=new Scanner(System.in); 
+      System.out.println("Enter the total purchase amount:");
+       Double totalPurchase = scanner.nextdouble();
+  
+    
+
+  int score=sc.nextInt();
+        
+if(total purchase >= 100)
+System.out.println("A");
+else if(score >= 80 && score <=89 )
+System.out.println("B");
+else if(score >= 70 && score <=79)
+System.out.println("C");
+else if(score >= 60 && score <=69)
+System.out.println("D");
+else if(score <= 60)
+System.out.println("F");
+    } 
+} 
