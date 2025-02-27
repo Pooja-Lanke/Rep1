2673b1fdcae75c26bb8f0d0146b6eab6772dfaf8
pooja Nichit <poojaknichit@gmail.com>
2025-02-27 23:37:26 +0530

    Add files via upload

---
+++ Assignments/Assignment1Lab/AreaOfRectangle.java
@@ -0,0 +1,10 @@
+class AreaOfRectangle{
+public static void main(String[] args){
+int length = 12;
+int  width = 14;
+int area = length * width;
+System.out.println("Area of Rectangle is: "+ area);
+
+}
+
+}
