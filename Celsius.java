124ec86db1fd7c1d84857924533943b93c1ba032
pooja Nichit <poojaknichit@gmail.com>
2025-02-27 20:29:50 +0530

    Delete Celsius.java

--- Celsius.java
+++
@@ -1,8 +0,0 @@
-class Celsius
-{
-public static void main(String[] args){
-double celcius = 25, fahrenheit=0.0;
-fahrenheit = (celcius * 9 / 5) + 32;
-System.out.println(celcius + " is equal to " + fahrenheit);
-}
-}
